53ae6b0697d5ad1c9486c49e28324b8a104b1b1b
lukezhg <lukezhg@gmail.com>
2018-08-30 11:39:34 +0800

    upload img

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd "cd ~/github/Freyja"
+cd "~/github/Freyja"
 git add --all
 git commit -m "upload img"
 git push origin master
 echo "https://github.com/lukezhg/Freyja/raw/master/"
 echo "upload finished"
-cd "~/octopress/"
+cd "~/octopress"
